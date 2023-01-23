46626431e473d20257d4e13e3b2b2e99716a2ede
Nathan Lucas <nathanlucaass.silva@gmail.com>
2023-01-23 19:45:38 -0300

    Sexto Exercicio

--- 01-Fundamentos/1.3-Introdução-à-JavaScript/DIA-01:-Switch-e-Case/Exercicio.js
+++ 01-Fundamentos/1.3-Introdução-à-JavaScript/DIA-01:-Switch-e-Case/Exercicio.js
@@ -81,3 +81,24 @@ switch (peça) {
         break;
 
 }
+
+//Sexto Exercicio
+let porcentagem = 75
+if(porcentagem >= 90){
+    console.log('A')
+}
+else if(porcentagem >= 80){
+    console.log('B')
+}
+else if(porcentagem >= 70){
+    console.log('C')
+}
+else if(porcentagem >= 60){
+    console.log('D')
+}
+else if(porcentagem >= 50){
+    console.log('E')
+}
+else{
+    console.log('F')
+}
