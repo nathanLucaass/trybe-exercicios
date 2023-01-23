8df5e4b166f2466239e6f596c0e76885a069d22e
Nathan Lucas <nathanlucaass.silva@gmail.com>
2023-01-23 19:38:22 -0300

    Metade do Exercicio

--- 01-Fundamentos/1.3-Introdução-à-JavaScript/DIA-01:-Switch-e-Case/Exercicio.js
+++ 01-Fundamentos/1.3-Introdução-à-JavaScript/DIA-01:-Switch-e-Case/Exercicio.js
@@ -1,21 +1,83 @@
-let resultado = 'aprovada'
-switch (resultado) {
-    case 'aprovada':
-        console.log('Parabéns, você foi aprovada(o)!');
+//Primeiro Exercicio
+const a = 4;
+const b = 2;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+//Segundo Exercicio
+const c = 3;
+const d = 5;
+const e = 6;
+
+if (c > d && c > e) {
+    console.log('C é maior')
+}
+else if (d > c && d > e) {
+    console.log('D é maior')
+}
+else {
+    console.log('E é maior')
+}
+
+//Terceiro Exercicio
+let parametro;
+if (parametro > 0) {
+    console.log('Positive');
+}
+else if (parametro < 0) {
+    console.log('Negative');
+}
+else {
+    console.log('zero');
+}
+
+//Quarto Exercicio
+const f = 60
+const g = 60
+const h = 60
+
+if ((f + g + h) === 180) {
+    console.log(true)
+}
+else {
+    console.log(false)
+}
+
+//Quinto Exercicio
+let peça;
+
+switch (peça) {
+    case 'Torre':
+        console.log('Move em linhas retas');
 
         break;
 
-    case 'lista':
-        consol.log('Você está na nossa lista de espera');
+    case 'Cavalo':
+        console.log('Move em L')
 
         break;
 
-    case 'reprovada':
-        consol.log('Você foi reprovada(o)');
+    case 'Bispo':
+        console.log('Move na diagonal')
 
         break;
 
-    default:
-        console.log('Informação incorreta')
+    case 'Rainha':
+        console.log('Move em linhas retas e na diagonal')
+
         break;
+
+    case 'Rei':
+        console.log('Move uma casa na diagonal ou em linha reta')
+
+        break;
+    case 'Peão':
+        console.log('Move uma ou duas casas da pra frente')
+
+        break;
+
 }
