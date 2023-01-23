fc01092dde6ecbbe0af36585c933b4e6f2ae2f07
Nathan Lucas <nathanlucaass.silva@gmail.com>
2023-01-23 16:14:33 -0300

    Primeira parte derminada

---
+++ 01-Fundamentos/1.3-Introdução-à-JavaScript/DIA-01:-Switch-e-Case/Exercicio.js
@@ -0,0 +1,21 @@
+let resultado = 'aprovada'
+switch (resultado) {
+    case 'aprovada':
+        console.log('Parabéns, você foi aprovada(o)!');
+
+        break;
+
+    case 'lista':
+        consol.log('Você está na nossa lista de espera');
+
+        break;
+
+    case 'reprovada':
+        consol.log('Você foi reprovada(o)');
+
+        break;
+
+    default:
+        console.log('Informação incorreta')
+        break;
+}
