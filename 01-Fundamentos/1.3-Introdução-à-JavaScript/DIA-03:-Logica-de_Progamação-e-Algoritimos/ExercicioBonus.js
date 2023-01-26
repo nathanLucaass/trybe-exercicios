3b0d53ca6e7b92e4b32bd3b1b5b15e9bce41338a
Nathan Lucas <nathanlucaass.silva@gmail.com>
2023-01-26 13:46:47 -0300

    Exercicio Bonus

---
+++ 01-Fundamentos/1.3-Introdução-à-JavaScript/DIA-03:-Logica-de_Progamação-e-Algoritimos/ExercicioBonus.js
@@ -0,0 +1,17 @@
+//Exercicio Bonus
+// Exercio
+let n = 5
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    inputLine += symbol;
+}
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    console.log(inputLine)
+}
+
+
+
+
