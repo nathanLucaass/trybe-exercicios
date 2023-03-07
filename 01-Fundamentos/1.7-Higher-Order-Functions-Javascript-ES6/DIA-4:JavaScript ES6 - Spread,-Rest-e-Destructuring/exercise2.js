8eab6412493071f6b356fccb90501a09eefdf9e9
nathanLucaass <nathanlucaass.silva@gmail.com>
2023-03-07 20:27:18 -0300

    Terceiro Requisito

--- 01-Fundamentos/1.7-Higher-Order-Functions-Javascript-ES6/DIA-4:JavaScript ES6 - Spread,-Rest-e-Destructuring/exercise2.js
+++ 01-Fundamentos/1.7-Higher-Order-Functions-Javascript-ES6/DIA-4:JavaScript ES6 - Spread,-Rest-e-Destructuring/exercise2.js
@@ -2,14 +2,14 @@
 //  retorna a soma desses parâmetros.
 //   Ao chamar a função dessa forma : sum(4,5,6), o número 15 deve ser retornado.
 
-// const sum = (...paramiters) => {
-//     let result = 0
-//     paramiters.forEach((paramiter) => {
-//         result += paramiter
-//     })
-//     return result
-// }
-// console.log(sum(4, 5, 6));
+const sum1 = (...paramiters) => {
+    let result = 0
+    paramiters.forEach((paramiter) => {
+        result += paramiter
+    })
+    return result
+}
+console.log(sum1(4, 5, 6));
 
-const sum = (...paramiters) => paramiters.reduce ((acc, curr) => acc + curr, 0);
-console.log(sum(4, 5, 6));
+const sum2 = (...paramiters) => paramiters.reduce ((acc, curr) => acc + curr, 0);
+console.log(sum2(4, 5, 6));
