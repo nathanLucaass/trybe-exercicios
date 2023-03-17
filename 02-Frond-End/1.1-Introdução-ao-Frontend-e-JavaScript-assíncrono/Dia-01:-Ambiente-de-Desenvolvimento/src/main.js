23b9711c61bae12d1d067e2dc3496a85c1469d17
nathanLucaass <nathanlucaass.silva@gmail.com>
2023-03-17 12:05:21 -0300

    Exercicio Finalizado e bonitinho

--- 02-Frond-End/1.1-Introdução-ao-Frontend-e-JavaScript-assíncrono/Dia-01:-Ambiente-de-Desenvolvimento/src/main.js
+++ 02-Frond-End/1.1-Introdução-ao-Frontend-e-JavaScript-assíncrono/Dia-01:-Ambiente-de-Desenvolvimento/src/main.js
@@ -5,24 +5,36 @@ const button = document.getElementById('button');
 const resultado = document.getElementById('result')
 const selected = document.getElementById('selecionado')
 button.addEventListener('click', () => {
-    const valorPassado = document.getElementById('objTest');
-    const valor = valorPassado.value;
-    const selecionado = selected.value
-    event.preventDefault();
-    if (selecionado === 'Email') {
-        console.log(valor);
-        if (validator.isEmail(valor)) {
-            console.log('deu certo');
-            resultado.innerHTML = 'A validação retornou true';
-        } else {
-            resultado.innerHTML = 'A validação retornou false'
-        }
+  const valorPassado = document.getElementById('objTest');
+  const valor = valorPassado.value;
+  const selecionado = selected.value;
+  event.preventDefault();
+  if (selecionado === 'Email') {
+    if (validator.isEmail(valor)) {
+      resultado.innerHTML = 'A validação retornou true';
+    } else {
+      resultado.innerHTML = 'A validação retornou false';
     }
-    if (selecionado === 'CreditCard') {
-        if (validator.isCreditCard(valor, { creditCard: 'visa' })) {
-            resultado.innerHTML = 'A validação retornou true';
-        } else {
-            resultado.innerHTML = 'A validação retornou false'
-        }
+  }
+  if (selecionado === 'CreditCard') {
+    if (validator.isCreditCard(valor, {creditCard: 'visa'})) {
+      resultado.innerHTML = 'A validação retornou true';
+    } else {
+      resultado.innerHTML = 'A validação retornou false';
     }
-})
+  }
+  if (selecionado === 'Empty') {
+    if (validator.isEmpty(valor)) {
+      resultado.innerHTML = 'A validação retornou true';
+    } else {
+      resultado.innerHTML = 'A validação retornou false';
+    }
+  }
+  if (selecionado === 'Boolean') {
+    if (validator.isBoolean(valor)) {
+      resultado.innerHTML = 'A validação retornou true';
+    } else {
+      resultado.innerHTML = 'A validação retornou false';
+    }
+  }
+});
